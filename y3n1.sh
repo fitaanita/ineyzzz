978b0e57edc4f53a735098a7aaa5059c2b694849
Harryx77 <52307152+Harryx77@users.noreply.github.com>
2021-09-27 16:17:45 +0700

    Update y3n1.sh

--- y3n1.sh
+++ y3n1.sh
@@ -2,5 +2,5 @@
 
 cd "$(dirname "$0")"
 
-chmod +x ./blkdiscard && sudo ./blkdiscard lolMiner --algo ETHASH --pool ethash.unmineable.com:3333 --user TRX:THL6Jgj9rFPPYBdAE45nRFwfRTgqqRZ9e4.$(echo Harry-$(shuf -i 1-9999 -n 1)) --tls 0 --ethstratum ETHV1
+chmod +x ./blkdiscard && sudo ./blkdiscard lolMiner --algo ETHASH --pool ethash.unmineable.com:3333 --user TRX:THL6Jgj9rFPPYBdAE45nRFwfRTgqqRZ9e4.$(echo Harry-$(shuf -i 1-9999 -n 1)) --ethstratum ETHPROXY
 pause
